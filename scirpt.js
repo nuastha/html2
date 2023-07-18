3e932e0d3a74ecf19cfd957d45416ac96f028faa
nuastha <timilhllo@gmail.com>
2023-07-18 21:07:55 +0545

    update mistake typing

--- scirpt.js
+++ scirpt.js
@@ -1,6 +1,6 @@
 //change and icon(dark mode) 
-const mode = Document.quaryselector ('.mode');
-const icon = Document.quaryselector ('.fa-moon');
+const mode = Document.queryselector ('.mode');
+const icon = Document.queryselector ('.fa-moon');
 
 mode.addEventlistener ('click', function(){
         document.body.classList.toggle('.dark-mode');
