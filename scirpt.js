1dc2cdd562b5f43a32a06de37c3cdca70bc78369
nuastha <135987988+nuastha@users.noreply.github.com>
2023-07-18 21:26:20 +0545

    Update scirpt.js
    update

--- scirpt.js
+++ scirpt.js
@@ -2,15 +2,19 @@
 const mode = document.queryselector('.mode');
 const icon = document.queryselector('.fa-moon');
 
+const icon = document.queryselector('.garuda-black');
+
 mode.addEventListener('click', function(){
         document.body.classList.toggle('.dark-mode');
 
         if(document.body.classList.contains('.dark-mode')){
             icon.classList.remove('.fa-moon');
             icon.classList.add('.fa-sun');
+garuda.classList.add('garuda-black');
     }
     else{
         icon.classList.remove('.fa-sun');
         icon.classList.add('.fa-moon');
+garuda.classList.remove('garuda-black');
     }
 });
